a1e75efbb0728df1f36dff08a6f04e1b608821b8
Songül Yapici <song0068@edu.zealand.dk>
2024-04-30 13:44:24 +0200

    Add project files.

---
+++ MusicREST/MusicRecords.cs
@@ -0,0 +1,14 @@
+﻿namespace MusicREST
+{
+    public class MusicRecords
+    {
+        public int Id {  get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set;}
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+
+
+
+    }
+}
---
+++ MusicREST/MusicRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicREST
+{
+    public class MusicRepository
+    {
+        private int _nextId = 4;
+        private List<MusicRecords> _musics = new()
+        {
+            new MusicRecords { Id = 1, Title = "Renaissance", Artist = "Beyonce", Duration = 16, PublicationYear = 2022 },
+            new MusicRecords { Id = 2, Title = "Pink Friday 2", Artist = "Nicki Minaj", Duration = 22, PublicationYear = 2023 },
+            new MusicRecords { Id = 3, Title = "Graduation", Artist = "Kanye West", Duration = 14, PublicationYear = 2007 }
+        };
+    }
+}
